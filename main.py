a9e41cb011cd9c75176a550dc5fd841c02e044e7
Mandq <165655071+xMandq@users.noreply.github.com>
2025-01-16 19:36:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ from yt_dlp import YoutubeDL
 import os
 
 playlist_url = 'https://www.youtube.com/playlist?list=PLRlwPks-XodI3CcKCpDaxlHQ9qB3R4ooc'
-
 desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop', 'Youtube_Downloads')
 os.makedirs(desktop_path, exist_ok=True)
-
 cookies_path = os.path.join(os.path.expanduser('~'), 'Desktop', 'cookies.txt')
+
+download_counter = {'count': 1}
+
+def custom_filename(info_dict):
+    title = info_dict.get('title', 'video')
+    filename = f"{download_counter['count']:03d}_{title}"
+    download_counter['count'] += 1
+    return filename
 
 ydl_opts = {
     'format': 'bestaudio/best',
@@ -15,10 +21,12 @@ ydl_opts = {
         'preferredcodec': 'mp3',
         'preferredquality': '192',
     }],
-    'outtmpl': os.path.join(desktop_path, '%(title)s.%(ext)s'),
+    'outtmpl': os.path.join(desktop_path, '%(autonumber)s_%(title)s.%(ext)s'),
     'ignoreerrors': True,
     'verbose': True,
-    'cookiefile': cookies_path
+    'cookiefile': cookies_path,
+    'autonumber_start': 1,
+    'autonumber_size': 3,  # This will create numbers like 001, 002, etc.
 }
 
 def download_playlist():
